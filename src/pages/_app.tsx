68933dd579c793d6810c55a1fb4fc111ee59a9c3
Yuto Mabe <yuto96315mabe@gmail.com>
2023-08-02 20:09:48 +0900

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>まべ五稜郭消化器・内視鏡クリニック</title>
+        <link rel="icon" href="/ロゴ最終盤.svg" />
+        {/* 他のメタ情報やCSSなどもここに追加することができます */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
