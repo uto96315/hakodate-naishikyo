3a71f21eac64c984ca12f150152d7dc7557a1650
Yuto Mabe <yuto96315mabe@gmail.com>
2023-08-05 16:48:48 +0900

    ヘッダーのレスポンシブ化

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 
 const Header = () => {
     return (
-        <div className="absolute top-0 left-0 bg-theme py-5 px-10 flex items-center justify-between w-1/4 z-50">
-            <Image
+        <div className="
+        w-screen bg-theme py-5 px-10 
+         md:absolute md:top-0 md:left-0 md:w-1/4 md:z-50
+        ">
+            <img
                 src="/logo-flex-white.png"
                 alt="まべ五稜郭・内視鏡クリニック"
-                objectFit="contain"
-                width={300}
-                height={50}
-                className="z-50"
+                className=""
             />
         </div>
     );
