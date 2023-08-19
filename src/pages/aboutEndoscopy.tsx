5e6bfc7444951b2605c54b5739058eaa53920ca1
Yuto Mabe <yuto96315mabe@gmail.com>
2023-08-19 22:30:01 +0900

    文言の修正

--- src/pages/aboutEndoscopy.tsx
+++ src/pages/aboutEndoscopy.tsx
@@ -16,7 +16,7 @@ const AboutEndoscopy: React.FC<Props> = () => {
 
                 <div className="flex flex-col lg:flex-row justify-center pb-10">
                     <div className="flex-1 text-center mt-10 lg:mt-0 lg:border-r-2">
-                        <h3 className="text-lg pb-3">上部内視鏡検査について</h3>
+                        <h3 className="text-lg pb-3">上部消化管内視鏡検査について</h3>
                         <div className="flex flex-col justify-center items-center">
                             <DownloadButton text="説明書ダウンロード" filepath="/上部内視鏡説明書.pdf" />
                             <DownloadButton text="同意書ダウンロード" filepath="/上部内視鏡同意書.pdf" />
@@ -25,15 +25,18 @@ const AboutEndoscopy: React.FC<Props> = () => {
                     </div>
 
                     <div className="flex-1 text-center">
-                        
+
                         <h3 className="text-lg pb-3">大腸内視鏡検査について</h3>
                         <div className="flex flex-col justify-center items-center">
                             <DownloadButton text="説明書ダウンロード" filepath="/大腸内視鏡説明書.pdf" />
                             <DownloadButton text="同意書ダウンロード" filepath="/大腸内視鏡同意書.pdf" />
+                            <p className="text-center px-20 pt-10">
+                                ※ 抗血栓薬服用中の方へ：大腸内視鏡では、原則抗血栓薬を当日朝のみ休薬にしていますが、念のため下記をダウンロードして印刷し、主治医に確認ください
+                            </p>
                             <DownloadButton text="抗血栓薬を服用中の方へ" filepath="/抗血栓薬休薬の可否について.pdf" />
                         </div>
                     </div>
-                    
+
                     {/* <div className="flex-1 text-center mt-10 lg:mt-0">
                         <h3 className="text-lg pb-3">抗血栓薬を服用中の方へ</h3>
                         <div className="flex flex-col justify-center items-center">
@@ -41,7 +44,6 @@ const AboutEndoscopy: React.FC<Props> = () => {
                         </div>
                     </div> */}
                 </div>
-
             </div>
         </div>
     );
