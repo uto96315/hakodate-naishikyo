2b13696faa77a080d8487366c0c6d19274a0df25
Yuto Mabe <yuto96315mabe@gmail.com>
2023-09-18 19:12:53 +0900

    メニュー修正

--- components/buttonAreaForSM.tsx
+++ components/buttonAreaForSM.tsx
@@ -1,6 +1,7 @@
 import { BiMap } from "react-icons/bi";
 import { HiDesktopComputer, HiOutlineClipboardList } from "react-icons/hi";
 import { BsFillTelephoneFill } from "react-icons/bs";
+import { jumpToSection } from "@/pages/aboutEndoscopy";
 
 
 
@@ -9,21 +10,18 @@ const ButtonAreaForSM = () => {
     const menus = [
         {
             title: "WEB問診",
-            icon: <BsFillTelephoneFill size={20} />,
-            bg: "",
-            link: "",
+            icon: <HiOutlineClipboardList size={20} />,
+            onClick: () => { window.open("https://symview.me/medical_interview_flows/hg07928/public/?url_kind=1"); }
         },
         {
             title: "WEB予約",
             icon: <HiDesktopComputer size={20} />,
-            bg: "",
-            link: "",
+            onClick: () => { window.open("https://ssc.doctorqube.com/hakodate-naishikyou/"); }
         },
         {
             title: "アクセス",
             icon: <BiMap size={20} color="white" />,
-            bg: "",
-            link: "",
+            onClick: () => { jumpToSection("access"); }
         },
     ];
     return (
@@ -33,7 +31,11 @@ const ButtonAreaForSM = () => {
         ">
             <div className="flex justify-between items-center space-x-1 px-3">
                 {menus.map((menu, index) => (
-                    <button key={index} className="w-1/3 border text-center flex items-center justify-center py-4 px-2 text-white bg-theme rounded-lg">
+                    <button
+                        key={index}
+                        onClick={menu.onClick}
+                        className="w-1/3 border text-center flex items-center justify-center py-4 px-2 text-white bg-theme rounded-lg"
+                    >
                         <div className="mr-1">
                             {menu.icon}
                         </div>
